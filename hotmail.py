8782cab497017360a815c75b91f02493e0292ce0
Gilmarfilho2003 <89646761+Gilmarfilho2003@users.noreply.github.com>
2021-09-24 16:57:59 -0300

    Update hotmail.py

--- hotmail.py
+++ hotmail.py
@@ -119,5 +119,7 @@ def proxy():
                 except smtplib.SMTPAuthenticationError:
                     print("Not Found Password : {} \t Email Hotmail:{}".format(password,options.hotmail))
    
- 
+ else:
+    print(use.usage)
+    exit()  
 ############################################################THE END####################################################################

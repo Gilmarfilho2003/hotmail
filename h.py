e768a1920185de97e3f8682d38ecf7313e86c6c2
Gilmarfilho2003 <89646761+Gilmarfilho2003@users.noreply.github.com>
2021-09-24 11:26:11 -0300

    Update h.py

--- h.py
+++ h.py
@@ -95,7 +95,7 @@ def proxy():
         checkProxyIP = brows.open("https://api.ipify.org/?format=raw", timeout=10)
     except:
         return proxy()
-    elif options.hotmail != None 
+    elif options.hotmail != true
         smtp_srverH= smtplib.SMTP('smtp.live.com', 587)
         smtp_srverH.ehlo()
         smtp_srverH.starttls()
